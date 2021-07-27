3c7fd446c2d127eb282a3dc0cd4cdec38f0891ad
Ali Sheikh <ali.sheikh@flexport.com>
2021-07-26 21:02:25 -0400

    new

--- ZK_equality.py
+++ ZK_equality.py
@@ -4,24 +4,28 @@ from zksk import utils
 def ZK_equality(G,H):
     r_1 = Secret(utils.get_random_num(bits=128))
     print(r_1)
-    r2 = Secret(utils.get_random_num(bits=128))
+    r_2 = Secret(utils.get_random_num(bits=128))
     print
     m = Secret(utils.get_random_num(bits=128))
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
     C1, C2 = elgamal(G, H, r_1, m)
-    D1, D2 = elgamal(G, H, r2, m)
+    D1, D2 = elgamal(G, H, r_2, m)
 
     #Generate a NIZK proving equality of the plaintexts
     #Define statement
-    statment = DLRep(C1, r_1*G) & DLRep(C2, r_1*H+m*G) & DLRep(D1, r2*G) & DLRep(D2, r2*H+m*G)
-
+    statment = DLRep(C1, G * r_1) & DLRep(C2, r_1 * H + m * G) & DLRep(D1, G * r_2) & DLRep(D2, r_2*H+m*G)
+    #Proof
+    print(statement)
     zk_proof = statment.prove()
 
     #Return two ciphertexts and the proof
     return (C1,C2), (D1,D2), zk_proof
 
 def elgamal(G, H, r, m):
-    x = r.value * G
-    y = r.value * H + m.value * G
+    #define x
+    x =  G * r.value
+    #print(x)
+    y =  m.value * G + r.value * H
+    #print(y)
     return x,y
